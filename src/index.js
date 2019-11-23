3e0d2eaa9b521f6587771d6d34e6e2c06a10eab5
trustyoo86 <trustyoo86.github.io>
2019-11-23 18:09:27 +0900

    Add README.md

--- src/index.js
+++ src/index.js
@@ -6,11 +6,20 @@
  * @param {Function} nextFn function for next
  * @return {Object} assemble objects
  */
-const pipeFn = (prevFn, nextFn) => (...args) => Object.assign(prevFn(...args), nextFn(...args));
+const objPipe = (prevFn, nextFn) => (...args) => Object.assign(prevFn(...args), nextFn(...args));
+
+
+cosnt pipe
 
 /**
  * assemble object with function and same parameters
  * @param {...Function} fns function list
  * @return {Object} Objects data after excute function
  */
-exports.pipe = const pipe = (...fns) => fns.reduce(pipeFn);
+const pipeFnsObject = (...fns) => fns.reduce(pipeFn);
+
+function pipe(type) {
+  return (...fns) => {
+    return 
+  };
+}
